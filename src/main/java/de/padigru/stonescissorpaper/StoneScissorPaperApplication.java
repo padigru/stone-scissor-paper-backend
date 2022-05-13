4b2b6a64b8614783a4b822769c0d36041c72db07
Patrick Grunau <p.d.g@hotmail.de>
2022-05-13 14:49:52 +0200

    Init Spring Boot project

---
+++ src/main/java/de/padigru/stonescissorpaper/StoneScissorPaperApplication.java
@@ -0,0 +1,13 @@
+package de.padigru.stonescissorpaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoneScissorPaperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoneScissorPaperApplication.class, args);
+	}
+
+}
